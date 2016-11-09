e636e8cd3e3ae5562aa18e5f68ab2283d4085bf0
Zachary Fairchild <Zachary Fairchild>
2016-11-09 13:45:46 -0500

    Changed DBURI to be mlab db

--- model/db.js
+++ model/db.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // Will be used to define a function to accept 
 // message and callback function
 var gracefulShutdown;
-var dbURI = 'mongodb://localhost/roselistdb';
+var dbURI = 'mongodb://admin:password@ds139567.mlab.com:39567/roselistdb';
 if (process.env.NODE_ENV === 'production'){
     dbURI = process.env.MLAB_URI;
 }
